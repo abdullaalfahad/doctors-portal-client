28f9e1a0b81cb778347d187625fb5018a91705cc
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-11 11:03:50 +0600

    info added

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InfoCard from '../InfoCard/InfoCard';
-import clock from '../../../assets/icons/clock.svg'
+import clock from '../../../assets/icons/clock.svg';
+import marker from '../../../assets/icons/marker.svg';
+import phone from '../../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard></InfoCard>
-            <InfoCard></InfoCard>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-12'>
+            <InfoCard title="Opening Hours" description="Lorem Ipsum is simply dummy text of the pri" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
+            <InfoCard title="Visit our location" description="Brooklyn, NY 10036, United States" bgClass="bg-accent" img={marker}></InfoCard>
+            <InfoCard title="Contact us now" description="+000 123 456789" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     );
 };
--- src/Pages/Home/InfoCard/InfoCard.js
+++ src/Pages/Home/InfoCard/InfoCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, title, description, bgClass }) => {
     return (
-        <div class="card lg:card-side bg-accent shadow-xl">
-            <figure><img src={img} alt="Album" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+        <div class={`card lg:card-side bg-accent shadow-xl text-white ${bgClass}`} >
+            <figure className='pl-5'><img src={img} alt="Album" /></figure>
+            <div class="card-body ">
+                <h2 class="card-title">{title}</h2>
+                <p>{description}</p>
             </div>
-        </div>
+        </div >
     );
 };
 
