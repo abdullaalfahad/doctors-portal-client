1f36ddaadc26980e13291f85054a09d3ccb497b8
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-19 20:26:46 +0600

    make admin added

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({ user }) => {
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                refetch();
+                toast.success('Admin added successFully');
+            })
+    }
+
     return (
         <tr>
             <th>1</th>
-            <td>{user.email}</td>
-            <td><button class="btn text-white">Make Admin</button></td>
+            <td>{email}</td>
+            <td>{role !== 'admin' && <button class="btn text-white" onClick={makeAdmin}>Make Admin</button>}</td>
             <td><button class="btn bg-red-500 border-0 text-white">Remove User</button></td>
         </tr>
     );
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -4,7 +4,7 @@ import Loading from '../Shared/Loading/Loading';
 import UserRow from './UserRow';
 
 const Users = () => {
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/users', {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/users', {
         method: 'GET',
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
@@ -28,7 +28,7 @@ const Users = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map(user => <UserRow key={user._id} user={user}></UserRow>)}
+                        {users.map(user => <UserRow key={user._id} user={user} refetch={refetch}></UserRow>)}
                     </tbody>
                 </table>
             </div>
