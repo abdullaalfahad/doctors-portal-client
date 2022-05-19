f2731701e8e628eb9ef478a351caea6399e4bc18
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-19 18:10:29 +0600

    verfiy JWT added

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,14 +1,29 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyAppointment = () => {
     const [appointments, setAppointments] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        fetch(`http://localhost:5000/booking?pateintEmail=${user.email}`)
-            .then(res => res.json())
+        fetch(`http://localhost:5000/booking?patientEmail=${user.email}`, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+        })
+            .then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    signOut(auth);
+                    localStorage.removeItem('accessTOken');
+                    navigate('/');
+                }
+                return res.json()
+            })
             .then(data => setAppointments(data))
     }, [])
 
