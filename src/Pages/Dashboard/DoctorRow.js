47ba808d2163d82ba3363df547811db5fb32ae1a
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-21 13:57:29 +0600

    delete doctor added

--- src/Pages/Dashboard/DoctorRow.js
+++ src/Pages/Dashboard/DoctorRow.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const DoctorRow = ({ doctor, index, refetch }) => {
     const { name, email, specialty, img } = doctor;
     const handleDelete = email => {
-        fetch()
+        fetch(`http://localhost:5000/doctor/${email}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount) {
+                    toast.success(`Doctor ${name} delete`);
+                }
+            })
     }
 
     return (
         <tr>
-            <th>{index}</th>
-            <td>avatar</td>
+            <th>{index + 1}</th>
+            <td>
+                <div class="avatar">
+                    <div class="w-12 rounded-xl">
+                        <img src={img} alt={name} />
+                    </div>
+                </div>
+            </td>
             <td>{name}</td>
             <td>{specialty}</td>
             <td><button class="btn btn-xs bg-red-500 text-white border-0" onClick={() => handleDelete(email)}>Delete</button></td>
